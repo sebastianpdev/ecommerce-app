2364107f56ab61c2672b8ebe311f12efab6948ed
jhoan.polania <sebastian.polania.dev@gmail.com>
2023-09-20 08:39:12 -0500

    Proyecto iniciado

---
+++ ms-payment/application/payment.app/src/main/java/org/jspapps/ecommerce/payment/app/App.java
@@ -0,0 +1,13 @@
+package org.jspapps.ecommerce.payment.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ms-payment/domain/payment.usecase/src/main/java/org/jspapps/ecommerce/payment/usecase/App.java
@@ -0,0 +1,13 @@
+package org.jspapps.ecommerce.payment.usecase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
